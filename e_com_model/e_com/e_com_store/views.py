19096c11f0f6702bad17c0221c13f8d5426fe98e
prodhanm <mustafizur.prodhan@gmail.com>
2024-01-18 14:08:47 -0500

    configured HttpResponse into html and configured secret key.

--- e_com_model/e_com/e_com_store/views.py
+++ e_com_model/e_com/e_com_store/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Hello, world. You're at the e_com_store.</h1>")
+    return render(request, 'home.html')
+    #return HttpResponse("<h1>Hello, world. You're at the e_com_store.</h1>")
+
+def about(request):
+    return render(request, 'about.html')
+    #return HttpResponse("<h1>About page</h1>")
